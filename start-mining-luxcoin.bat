1ef080c541e1f654408bbc0565170af8b5bfb226
pppnnn033 <41576450+pppnnn033@users.noreply.github.com>
2021-09-03 04:05:57 -1200

    Add files via upload

---
+++ start-mining-luxcoin.bat
@@ -0,0 +1,9 @@
+:: This is an example you can edit and use
+:: There are numerous parameters you can set, please check Help and Examples folder
+
+@echo off
+cd %~dp0
+cls
+
+SRBMiner-MULTI.exe --disable-gpu --algorithm rx2 --pool pool.letshash.it:9009 --wallet LezrEuXhSA6qSP3cAAnsg56brVzb5PQox4
+pause
